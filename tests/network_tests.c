35da3d2b6e940b7860ca3680e8013bd92df85fc7
d-soni <devinsoni1010@gmail.com>
2016-12-29 17:15:10 -0500

    update network tests

--- tests/network_tests.c
+++ tests/network_tests.c
@@ -12,18 +12,22 @@ int main(){
     assert(network->layers[2]->size == 4);
 
     // test forward pass
-    double** example_data = (double**)malloc(sizeof(double*));
+    double** example_data = (double**)malloc(sizeof(double*) * 2);
     example_data[0] = (double*)malloc(sizeof(double) * 5);
+    example_data[1] = (double*)malloc(sizeof(double) * 5);
     int i;
     for (i = 0; i < 5; i++){
         example_data[0][i] = (i + 1.0) / 2;
+        example_data[1][i] = (i + 1.5) / 2;
     }
-    Matrix* example = createMatrix(1, 5, example_data);
+    Matrix* example = createMatrix(2, 5, example_data);
     forwardPass(network, example);
 
     // test prediction
     int* prediction = predict(network);
     assert(prediction[0] >= 0 && prediction[0] <= 3);
+    assert(prediction[1] >= 0 && prediction[1] <= 3);
+    free(prediction);
 
     // test destroy
     destroyMatrix(example);
